2debfa46d96d5e440db2ebccb9bb00bd2a7ccf92
atmz <alex.toumazis@gmail.com>
2023-12-01 14:10:59 +0200

    Update code.py

--- 01/fast/code.py
+++ 01/fast/code.py
@@ -1,33 +1,6 @@
 import sys; sys.dont_write_bytecode = True; from util import *
-"""
-To do: ensure Code Runner works (in WSL), have preloaded the day and input in Chrome,
-saved input into the folder, have utils on the side, collapse regions
-Strings, lists, dicts:
-lmap, ints, positive_ints, floats, positive_floats, words, keyvalues
 
-Algorithms:
-bisect, binary_search, hamming_distance, edit_distance
 
-Data structures:
-Linked, UnionFind
-use deque for queue: q[0], q.append and q.popleft
-
-List/Vector operations:
-GRID_DELTA, OCT_DELTA
-lget, lset, fst, snd
-padd, pneg, psub, pmul, pdot, pdist1, pdist2sq, pdist2
-
-Matrices:
-matmat, matvec, matexp
-
-Previous problems:
-knot
-
-Dict things:
-dict.keys()
-dict.values()
-dict.items()
-"""
 def do_case(inp: str, sample=False):
     def sprint(*a, **k): sample and print(*a, **k)
     lines = inp.splitlines()
