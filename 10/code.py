f6133bdd583326b1657ebcce9cfccbbc6cf95da7
atmz <alex.toumazis@gmail.com>
2023-12-11 11:08:38 +0200

    Part 2 day 10

--- 10/code.py
+++ 10/code.py
@@ -20,9 +20,6 @@ def possible_moves(c):
         return (  )
 
 def search(start, goal, seen, map, distance):
-    # print(f"search({start}, {goal}, {seen}, map, {distance})")
-    # print(f"value:{map[start[0]][start[1]]}")
-    # print(f"possible_moves:{possible_moves(map[start[0]][start[1]])}")
     max_y =  len(map)
     max_x =  len(map[0])
     seen.add(start)
@@ -40,25 +37,39 @@ def search(start, goal, seen, map, distance):
                 path.append(start)
                 return (distance, path)
     
+
+def calculate_s(point, path):
+    for c in list("I-LJ7F"):
+        all_in_path = True
+        for (dy,dx) in possible_moves(point):
+            y = point[0]+dy
+            x = point[1]+dx
+            if (y,x) not in path:
+                all_in_path = False
+            if all_in_path:
+                return c
+
 def is_inside( point, path, lines):
-    if point in path:
-        return False
-    crossings = 0
-    last_was_crossing = False
-    y = point[0]
-    while y>0:
-        y-=1
-        if (y,point[1]) in path and lines[y][point[1]] in ['-','7','F']:
-            print(f"point{point} - crossing{(y,point[1])}")
-            if not last_was_crossing:
-                crossings+=1
-            last_was_crossing = True
-        else:
-            last_was_crossing = False
-    if crossings % 2 == 1:
-        print(f"inside - {crossings} - {point}")
-        return True
-    return False
+    x = point[1]
+    track_from_top = [lines[y][x] for y in range(0,point[0])]
+    outside = True
+    vertical_pipe_start = ''
+    for y,c in enumerate(track_from_top):
+        if c == 'S':
+            c = calculate_s( (y,x), path)
+        if c == '-'  and (y,x) in path:
+            outside = not outside
+        if c in ['7', 'F'] and (y,x) in path:
+            vertical_pipe_start = c
+        if c == 'L' and (y,x) in path:
+            if vertical_pipe_start == '7':
+                outside = not outside
+            vertical_pipe_start = ''
+        if c == 'J' and (y,x) in path:
+            if vertical_pipe_start == 'F':
+                outside = not outside
+            vertical_pipe_start = ''
+    return not outside
 
 
 def print_grid(grid):
@@ -70,8 +81,6 @@ def do_case(inp: str, sample=False):
     def sprint(*a, **k): sample and print(*a, **k)
     lines = inp.splitlines()
 
-    defaultdict_int = defaultdict(int)
-    score = 0
     max_y =  len(lines)
     max_x =  len(lines[0])
 
@@ -84,23 +93,28 @@ def do_case(inp: str, sample=False):
     seen = set()
     distance, path = search(start, start, seen, lines, 0)
     print(f"Part 1: {int(distance)}")
-    score = 0
+
+
     path = set(path)
+    inside = set()
     print(path)
+    printable_grid = copy.deepcopy(lines)
     for y in range(0, max_y):
-        for x in range(0, max_x):
-            if is_inside((y,x), path, lines):
-                score+=1
-    print(f"Part 2: {int(score)}")
-    
-    for y in range(0, max_y):
+        printable_grid[y]  = list(printable_grid[y])
         lines[y] = list(lines[y])
         for x in range(0, max_x):
-            if not (y,x) in path:
-                lines[y][x]='.'
+            if (y,x) in path:
+                printable_grid[y][x]='*'
+            elif is_inside((y,x), path, lines):
+                inside.add((y,x))
+                printable_grid[y][x] = 'I'
             else:
-                lines[y][x]=' '
-    print_grid(lines)
+                printable_grid[y][x] = 'O' 
+                
+    print_grid(printable_grid)
+    print(inside)
+
+    print(f"Part 2: {len(inside)}")
     return  # RETURNED VALUE DOESN'T DO ANYTHING, PRINT THINGS INSTEAD
 
 
@@ -126,15 +140,17 @@ LJ.LJ
 
 """],[
 # Part 2
-r"""...........
-.S-------7.
-.|F-----7|.
-.||.....||.
-.||.....||.
-.|L-7.F-J|.
-.|..|.|..|.
-.L--J.L--J.
-...........
+r"""FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L
+
 
 
 """,r"""
